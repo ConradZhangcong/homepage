d51e33d719a3ea430b06ad475cdec15dbe7a43f0
ConradZhangcong <heisenberg0519@outlook.com>
2022-05-05 15:16:54 +0800

    chore: add sass config

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./index.module.scss";
+
+const Home = () => {
+  return (
+    <>
+      <div className={styles.test}>Hi</div>
+      <div>Lazy</div>
+    </>
+  );
+};
+
+export default Home;
